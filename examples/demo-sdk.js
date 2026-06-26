c45bfc7b6f2ea09febbef7247930893dd182b8ec
sarias <sarias980@gmail.com>
2026-06-26 16:55:42 +0200

    Update userId generation to use current date in 'DD/MM/YYYY' format

--- examples/demo-sdk.js
+++ examples/demo-sdk.js
@@ -13,7 +13,7 @@ export async function initDemoSdk({modeLabelEl, eventLogEl} = {}) {
     sdk.init({
         debug: true,
         apiKey: 'jGeIP9g7Dyjf5HYAFDM8ozxZqQSiQMfJ',
-        userId: '020629',
+        userId: new Date().toLocaleDateString('es-ES', {year: 'numeric', month: '2-digit', day: '2-digit'}).split('/').reverse().join(''),
         analytics: {
             publicKey: '5a148214cdd4d164b9ff189c201d0e75',
             integration: 'a0365bd0-6ee1-11f1-94c7-45c08829a73b'
